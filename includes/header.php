3ba08370ac74125e6a05b9b381bdc9ca001c9c38
kamiyakoh <nanayou791@gmail.com>
2022-05-13 22:02:56 +0900

    最終チェック後修正

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php wp_body_open(); ?>
 
 <header class="l-header c-bg--lightCream">
-    <button class="c-headingR p-header__menuBtn js-btn__sidemenu">Menu</button>
+    <button class="c-headingR c-txtColor__gray p-header__menuBtn js-btn__sidemenu">Menu</button>
     <h2 class="c-headingR p-header__ttl">
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
     </h2>
